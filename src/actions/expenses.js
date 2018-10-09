617535fab18cf7fec08dce235a11e67ed74bba3d
Angelica <angelica@nisperolabs.com>
2018-10-09 07:50:22 -0600

    Remove data from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -36,6 +36,16 @@ export const removeExpense = ({ id } = {}) => ({
   id 
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+    return(dispatch) => {
+        return database.ref(`expenses/${id}`)
+            .remove()
+            .then(() => {
+                dispatch(removeExpense({ id }))
+            });
+    }
+};
+
 //EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -44,7 +54,6 @@ export const editExpense = (id, updates) => ({
 });
 
 //SET_EXPENSES
-
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
     expenses
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase'
 
@@ -24,6 +24,23 @@ test('Should set up REMOVE expense action object', () => {
         id: '123abc'
     });
 });
+
+test('should remove expense from firebase', (done) => {
+    const store = createMockStore({});
+    store.dispatch(startRemoveExpense({id: expenses[0].id}))
+    .then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id: expenses[0].id
+        });
+        return database.ref(`expenses/${actions[0].id}`).once('value');
+        })
+    .then((snapshot) => {
+            expect(snapshot.val()).toBeFalsy();
+            done();
+        });
+});    
 
 test('Should set up EDIT expense action object', () => {
     const action = editExpense( '123abc', {note:'Rent', amount:4000} );
