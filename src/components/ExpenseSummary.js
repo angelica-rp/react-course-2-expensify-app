0c71521981e24ec24e5bce1548df5f3ee9278de2
Angelica <angelica@nisperolabs.com>
2018-10-14 12:35:22 -0600

    Styling application

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import numeral from 'numeral';
 import getExpensesTotal from '../selectors/expenses-total';
 import getVisibleExpenses from '../selectors/expenses';
-import numeral from 'numeral';
-import { connect } from 'react-redux';
+
 
 export const ExpenseSummary = (props) => {
     const expenseWord = props.totalCount === 1 ? 'expense' : 'expenses' ;
     const formattedExpensesTotal = numeral(props.totalAmount / 100).format('$0,000.00');
     return (
-        <div>
-            <h2>Viewing {props.totalCount} {expenseWord} totalling {formattedExpensesTotal}</h2>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{props.totalCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal} </span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>    
+            </div>
         </div>
     );
 };
-
+ 
 const mapStateToProps = (state) => {
     const expenses= getVisibleExpenses(state.expenses, state.filters);
     return {
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,9 +3,13 @@ import { connect } from 'react-redux';
 import { startLoginGoogle, startLoginFacebook } from '../actions/auth';
 
 export const LoginPage = ({ startLoginGoogle, startLoginFacebook }) => (
-    <div>
-        <button onClick={startLoginGoogle}> Login Google</button>
-        <button onClick={startLoginFacebook}> Login Facebook</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expensify Application</h1>
+            <p>It's time to get your expenses under control.</p>
+            <button className="button button--login" onClick={startLoginGoogle}>Login with Google</button>
+            <button className="button button--login" onClick={startLoginFacebook}>Login with Facebook</button>
+        </div>
     </div>    
 );
 
